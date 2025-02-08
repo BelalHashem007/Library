1913ab79539df692c4b983dfb37eae2f31cebb6f
BelalHashem007 <belal.hashem.saleh@gmail.com>
2025-02-08 17:38:37 +0200

    Changing constructor to class

--- script.js
+++ script.js
@@ -1,87 +1,89 @@
 let myLibrary = [];
 
-function Book(title, author, pages, readStatus) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.readStatus = readStatus;
+class Book {
+
+    constructor(title, author, pages, readStatus) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.readStatus = readStatus;
+    }
+
+    changeReadStatus() {
+        if (this.readStatus == 'read')
+            this.readStatus = 'not read yet'
+        else
+            this.readStatus = 'read';
+    }
+
+    displayLastBook() {
+        const tBody = document.querySelector("tbody")
+        const tr = document.createElement('tr');
+        tBody.appendChild(tr);
+        let len = myLibrary.length
+
+        const td = document.createElement('td');
+        td.textContent = myLibrary[len - 1].title;
+        tr.appendChild(td);
+
+        const td2 = document.createElement('td');
+        td2.textContent = myLibrary[len - 1].author;
+        tr.appendChild(td2);
+
+        const td3 = document.createElement('td');
+        td3.textContent = myLibrary[len - 1].pages;
+        tr.appendChild(td3);
+
+        const td4 = document.createElement('td');
+        td4.textContent = myLibrary[len - 1].readStatus;
+        tr.appendChild(td4);
+
+        const changeStatusBtn = document.createElement('button');
+        changeStatusBtn.classList.add('switch-btn')
+        changeStatusBtn.textContent = 'Change';
+        const td5 = document.createElement('td');
+        td5.appendChild(changeStatusBtn);
+        tr.appendChild(td5);
+        changeStatusBtn.addEventListener('click', () => {
+            let rowIndex = changeStatusBtn.parentNode.parentNode.rowIndex;
+            myLibrary[rowIndex - 1].changeReadStatus();
+            let cell = document.querySelector('.table').rows[rowIndex].cells[3];
+            if (cell.textContent == 'read')
+                cell.textContent = 'not yet read'
+            else
+                cell.textContent = 'read'
+        })
+
+        const removeBookBtn = document.createElement('button');
+        removeBookBtn.classList.add('remove-btn')
+        removeBookBtn.textContent = 'Remove';
+        const td6 = document.createElement('td');
+        td6.appendChild(removeBookBtn);
+        tr.appendChild(td6);
+        removeBookBtn.addEventListener('click', () => {
+            let rowIndex = removeBookBtn.parentNode.parentNode.rowIndex;
+            document.querySelector('.table').deleteRow(rowIndex);
+            myLibrary = myLibrary.filter((book) => {
+                if (book == myLibrary[rowIndex - 1])
+                    return false;
+                return true;
+            })
+        })
+    }
 }
-Book.prototype.changeReadStatus = function (){
-    if (this.readStatus == 'read')
-        this.readStatus = 'not read yet'
-    else
-    this.readStatus = 'read';
-};
 
 function addBookToLibrary(title, author, pages, readStatus) {
-    myLibrary.push(new Book(title, author, pages, readStatus));
+    const book = new Book(title, author, pages, readStatus)
+    myLibrary.push(book);
+    return book;
 }
 
 
-function displayLastBook() {
-    const tBody = document.querySelector("tbody")
-    const tr = document.createElement('tr');
-    tBody.appendChild(tr);
-    let len = myLibrary.length
 
-    const td = document.createElement('td');
-    td.textContent = myLibrary[len-1].title;
-    tr.appendChild(td);
-
-    const td2 = document.createElement('td');
-    td2.textContent = myLibrary[len-1].author;
-    tr.appendChild(td2);
-
-    const td3 = document.createElement('td');
-    td3.textContent = myLibrary[len-1].pages;
-    tr.appendChild(td3);
-
-    const td4 = document.createElement('td');
-    td4.textContent = myLibrary[len-1].readStatus;
-    tr.appendChild(td4);
-
-    const changeStatusBtn = document.createElement('button');
-    changeStatusBtn.classList.add('switch-btn')
-    changeStatusBtn.textContent = 'Change';
-    const td5 = document.createElement('td');
-    td5.appendChild(changeStatusBtn);
-    tr.appendChild(td5);
-    changeStatusBtn.addEventListener('click', () => {
-        let rowIndex = changeStatusBtn.parentNode.parentNode.rowIndex;
-        myLibrary[rowIndex-1].changeReadStatus();
-        let cell = document.querySelector('.table').rows[rowIndex].cells[3];
-        if (cell.textContent == 'read')
-            cell.textContent = 'not yet read'
-        else
-            cell.textContent = 'read'
-    })
-    
-    const removeBookBtn = document.createElement('button');
-    removeBookBtn.classList.add('remove-btn')
-    removeBookBtn.textContent = 'Remove';
-    const td6 = document.createElement('td');
-    td6.appendChild(removeBookBtn);
-    tr.appendChild(td6);
-    removeBookBtn.addEventListener('click', () => {
-        let rowIndex = removeBookBtn.parentNode.parentNode.rowIndex;
-        document.querySelector('.table').deleteRow(rowIndex);
-        myLibrary = myLibrary.filter((book) => {
-            if (book == myLibrary[rowIndex-1])
-                return false;
-            return true;
-        })
-    } )
-}
-
-
-addBookToLibrary("A Story Of Yesterday", "Sergio Cobo", "70 Pages", "not yet read");
-displayLastBook();
-addBookToLibrary("To Kill a Mockingbird", "Harper Lee", "323 Pages", "read");
-displayLastBook();
-addBookToLibrary("Something Wicked This Way Comes", "Ray Bradbury", "293 Pages", "not yet read");
-displayLastBook();
-addBookToLibrary("A Clockwork Orange", "Anthony Burgess", "240 Pages", "read");
-displayLastBook();
+addBookToLibrary("A Story Of Yesterday", "Sergio Cobo", "70 Pages", "not yet read").displayLastBook();
+addBookToLibrary("To Kill a Mockingbird", "Harper Lee", "323 Pages", "read").displayLastBook();
+addBookToLibrary("Something Wicked This Way Comes", "Ray Bradbury", "293 Pages", "not yet read").displayLastBook();
+addBookToLibrary("A Clockwork Orange", "Anthony Burgess", "240 Pages", "read").displayLastBook();
 
 let form = document.forms["my-form"];
 form.addEventListener("submit", getValues);
@@ -95,8 +97,7 @@ function getValues(event) {
         "pages": this.pages.value,
         "readStatus": this.readStatus.value,
     }
-    addBookToLibrary(formData.title, formData.author, formData.pages, formData.readStatus);
-    displayLastBook();
+    addBookToLibrary(formData.title, formData.author, formData.pages, formData.readStatus).displayLastBook();
 }
 
 let dialBtn = document.querySelector('.dialog-btn');
